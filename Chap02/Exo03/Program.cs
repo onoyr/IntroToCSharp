37eab6d0ff8347318fa4315fa24319ef4a89a770
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-15 22:54:54 +0300

    Done exo 03

---
+++ Chap02/Exo03/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exo03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal a = 1.0000001m;
+        decimal b = 1m;
+        bool equals = Math.Abs(a - b) < 0.000001m;
+        Console.WriteLine(equals);
+    }
+}
