28909a16aaaa0f2a8426ffb0879118e842377dbc
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-15 21:44:18 +0300

    Done exo 08

---
+++ Chap01/Exo08/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Exo08;
+class Program
+{
+    // Write a program that prints on the console the current date and time.
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DateTime.Now.ToString());
+    }
+}
