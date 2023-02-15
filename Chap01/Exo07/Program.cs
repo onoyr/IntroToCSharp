d9aa66d074a322f4fa47634e5574c91bbae81b8a
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-15 21:41:40 +0300

    Done exo 07

---
+++ Chap01/Exo07/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exo07;
+class Program
+{
+    // Write a program that prints the following numbers on the console 1,
+    // 101, 1001, each on a new line.
+    static void Main(string[] args)
+    {
+        Console.WriteLine("101");
+        Console.WriteLine("1001");
+    }
+}
