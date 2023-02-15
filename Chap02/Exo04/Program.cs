28a5c95475f6107c895fee0defbb57ec97fc9c46
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-15 22:58:00 +0300

    Done exo 04

---
+++ Chap02/Exo04/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exo04;
+class Program
+{
+    // Initialize a variable of type int with a value of 256
+    // hexadecimal format (256 is 100 in a numeral system with base 16).
+    static void Main(string[] args)
+    {
+        int inHex = 0x100;
+        Console.WriteLine(inHex);
+    }
+}
