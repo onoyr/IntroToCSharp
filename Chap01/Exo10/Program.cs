4e1458d0480297cd4bb14f775739f4abc134450a
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-15 21:52:54 +0300

    Add exo 10

---
+++ Chap01/Exo10/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exo10;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var n = 2;
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (n % 2 != 0)
+            {
+                Console.WriteLine(n * -1);
+            }
+            else
+            {
+                Console.WriteLine(n);
+            }
+            n++;
+        }
+    }
+}
