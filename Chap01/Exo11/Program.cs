c81839e4680fc3feda491cd8f615b0decfc4f206
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-15 21:59:38 +0300

    Done exo 11

---
+++ Chap01/Exo11/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exo11;
+class Program
+{
+    // Write a program that reads your age from the console and prints your
+    // age after 10 years.
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        var s = Console.ReadLine();
+        if (s == null)
+        {
+            Console.WriteLine("You entered an invalid age");
+            return;
+        }
+        var age = int.Parse(s);
+        Console.WriteLine("After 10 years you will be " + (age + 10) + " years old");
+    }
+}
