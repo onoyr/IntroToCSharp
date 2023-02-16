c43f89e0fcc7ab7b460de2ba2394c7ce72617376
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 12:41:03 +0300

    Done exo 06

---
+++ Chap02/Exo06/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exo06;
+class Program
+{
+    // Declare a variable isMale of type bool and assign a value to it depending
+    // on your gender. 
+    static void Main(string[] args)
+    {
+        bool isMale = true;
+        Console.WriteLine("is male: " + isMale);
+    }
+}
