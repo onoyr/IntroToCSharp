a95dcbec64036af41b1f05b562473daf76420946
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 13:00:05 +0300

    Done exo 9

---
+++ Chap02/Exo09/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exo09;
+class Program
+{
+    // Declare two variables of type string and assign them a value “The
+    // "use" of quotations causes difficulties.” (without the outer quotes).
+    // In one of the variables use quoted string and in the other do not use it.
+    static void Main(string[] args)
+    {
+        string withQuotes = "The \"use\" of quotations causes difficulties.";
+        string withoutQuotes = @"The ""use"" of quotations causes difficulties.";
+        Console.WriteLine(withQuotes);
+        Console.WriteLine(withoutQuotes);
+    }
+}
