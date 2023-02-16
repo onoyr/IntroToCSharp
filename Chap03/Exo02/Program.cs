a12e5154166ff7a746e593ccb0dda5ef18895d27
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 17:30:49 +0300

    Done exo 02

---
+++ Chap03/Exo02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exo02;
+class Program
+{
+    // Write a Boolean expression that checks whether a given integer is
+    // divisible by both 5 and 7, without a remainder.
+    static void Main(string[] args)
+    {
+        int n = 35;
+        bool divisible = (n % 5 == 0) && (n % 7 == 0);
+        Console.WriteLine("n = " + n + " is divisible by 5 and 7: " + divisible);
+    }
+}
