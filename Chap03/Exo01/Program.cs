fa03b86ccd1f278519d3980b200f216c038ccd02
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 17:27:38 +0300

    Done exo 01

---
+++ Chap03/Exo01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exo01;
+class Program
+{
+    // Write an expression that checks whether an integer is odd or even.
+    static void Main(string[] args)
+    {
+        int n = 11;
+        bool isOdd = n % 2 != 0;
+        Console.WriteLine("n = " + n + " is odd: " + isOdd);
+    }
+}
