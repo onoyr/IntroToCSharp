6bf7b7b7822b22bdbe9823b028aa68f4bc2fca0d
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 15:19:21 +0300

    Done exo 10

---
+++ Chap02/Exo10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exo10;
+class Program
+{
+    // 10. Write a program to print a figure in the shape of a heart by the sign "o".
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" o       o");
+        Console.WriteLine("o   o o   o");
+        Console.WriteLine("o    o    o");
+        Console.WriteLine(" o       o");
+        Console.WriteLine("  o     o");
+        Console.WriteLine("   o   o");
+        Console.WriteLine("    o o");
+        Console.WriteLine("     o");
+    }
+}
