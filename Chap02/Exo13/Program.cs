7e4308006b4f613e9b4e53b28f314ea267ee91d6
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 15:38:38 +0300

    Done exo 13

---
+++ Chap02/Exo13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exo13;
+class Program
+{
+    // Declare two variables of type int. Assign to them values 5 and 10
+    // respectively. Exchange (swap) their values and print them.
+    static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 6;
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+
+        Console.WriteLine("==SWP==");
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+    }
+}
