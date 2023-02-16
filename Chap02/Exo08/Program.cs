f426888e7fb9a7eda6aa2a6d15f3c803586d6955
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 12:49:49 +0300

    Done exo 08

---
+++ Chap02/Exo08/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exo08;
+class Program
+{
+    // Declare two variables of type string and give them values "Hello" and
+    // "World". Assign the value obtained by the concatenation of the two
+    // variables of type string (do not miss the space in the middle) to a
+    // variable of type object. Declare a third variable of type string and
+    // initialize it with the value of the variable of type object (you should use
+    // type casting).
+    static void Main(string[] args)
+    {
+        string hello = "Hello";
+        string world = "World";
+        object helloWorldObj = hello + " " + world;
+        string helloWorld = (string) helloWorldObj;
+
+        Console.WriteLine(helloWorld);
+    }
+}
