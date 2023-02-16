76fe16eb25b475aea78516aecc262b1a52173999
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 15:31:34 +0300

    Done exo 12

---
+++ Chap02/Exo12/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exo12;
+
+using System;
+
+class Program
+{
+    // A company dealing with marketing wants to keep a data record of its
+    // employees. Each record should have the following characteristic – first
+    // name, last name, age, gender (‘m’ or ‘f’) and unique employee number
+    // (27560000 to 27569999). Declare appropriate variables needed to
+    // maintain the information for an employee by using the appropriate data
+    // types and attribute names.
+    static void Main(string[] args)
+    {
+        string firstName = "Sitraka";
+        string lastName = "Ratsimba";
+        byte age = 32;
+        char gender = 'm';
+        uint id = 26560001;
+
+        Console.WriteLine("First name: " + firstName);
+        Console.WriteLine("Last name: " + lastName);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Gender: " + gender);
+        Console.WriteLine("Id: " + id);
+    }
+}
