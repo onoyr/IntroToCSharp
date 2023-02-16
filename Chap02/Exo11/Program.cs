839f4eae90d8354aff9381492f04310d837e161c
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 15:22:13 +0300

    Done exo 11

---
+++ Chap02/Exo11/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exo11;
+class Program
+{
+    // Write a program that prints on the console isosceles triangle which
+    // sides consist of the copyright character "©".
+    static void Main(string[] args)
+    {
+        Console.WriteLine("    ©");
+        Console.WriteLine("   ©©©");
+        Console.WriteLine("  ©©©©©");
+        Console.WriteLine(" ©©©©©©©");
+        Console.WriteLine("©©©©©©©©©");
+    }
+}
