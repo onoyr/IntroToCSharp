81981efb3879fa76c4e0ecab649cb7ec891fa32c
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2023-02-16 12:44:44 +0300

    Done exo 07

---
+++ Chap02/Exo07/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exo07;
+class Program
+{
+    // Declare two variables of type string with values "Hello" and "World".
+    // Declare a variable of type object. Assign the value obtained of
+    // concatenation of the two string variables (add space if necessary) to this
+    // variable. Print the variable of type object
+    static void Main(string[] args)
+    {
+        string hello = "Hello";
+        string world = "World";
+        object helloWorld = hello + " " + world;
+        Console.WriteLine(helloWorld);
+    }
+}
